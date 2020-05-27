af80909fe9b4bc59294d28bec416f0964b45c923
Countersem <countersem@gmail.com>
2020-05-27 17:39:17 +0600

    shto upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	a = 1;
+	b = 0.02;
+	
+	$("#itemjoppa").click(function(){
+		if (a <= 0.04){
+			audio = new Audio('bang.mp3');
+			audio.play();
+			$(this).remove();
+			document.getElementById("itemjoppes").innerHTML="Жопа сгорела...";
+			document.getElementById("joppescaption").innerHTML="Вы пошатнули мироздание.";
+		}
+		else {
+			a -= b;
+			$(this).css({
+			"transform": "scale(" + a + ", " + a + ")"
+			});
+		}
+		
+	});
+});
